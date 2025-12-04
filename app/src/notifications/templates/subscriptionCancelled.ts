9ba75a0a339240cf972ec961aee0eb5ec2765c86
tomerg91 <loomcoachingplatform@gmail.com>
2025-12-04 11:53:59 +0200

    Fix TypeScript errors in notification templates and apply DB migration

--- app/src/notifications/templates/subscriptionCancelled.ts
+++ app/src/notifications/templates/subscriptionCancelled.ts
@@ -105,5 +105,5 @@ function escapeHtml(text: string | undefined): string {
     '"': "&quot;",
     "'": "&#039;",
   };
-  return text.replace(/[&<>"']/g, (char) => map[char]);
+  return text.replace(/[&<>"']/g, (char) => map[char] || char);
 }
